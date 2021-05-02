f3ac9e3e5956b848e0a4791cc33a264266cb34f5
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-02 13:23:22 +0100

    initial commit

---
+++ users/forms.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+from django import forms
+from .models import Profile
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username', 'password1', 'password2')
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Email or Username')
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username','email', 'first_name', 'last_name')
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('image','about', 'website_url')
+
