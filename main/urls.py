5a3abd497f7206c03b2890f1ed24150673afac74
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-04 15:36:38 +0100

    added more routes

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('notifications/', views.notifications, name='notifications'),
     path('messages/', views.messages, name='messages'),
+    path('paint/', views.paint, name='paint'),
+    path('', views.IndexListView.as_view(), name='index'),
+    path('add_paint/', views.AddPaintingView.as_view(), name='add_paint'),
+    path('view_paint/<int:pk>/<slug:paint>/', views.PaintingDetailView.as_view(), name='view_paint'),
 ]
