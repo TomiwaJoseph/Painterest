3c2a1df2b95926f62c600f1bfd4060e195989ce5
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-05 23:03:02 +0100

    added some more views

--- users/views.py
+++ users/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth import views as auth_views
 from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
-
 from .forms import LoginForm, RegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth import logout
+from .models import CustomUser, UserFollowing
+from django.contrib import messages
+
 
 # Create your views here.
 class LoginView(auth_views.LoginView):
@@ -18,8 +20,43 @@ class RegisterView(generic.CreateView):
     success_url = reverse_lazy('login')
 
 
+def following_and_follower(request, painter):
+    return render(request, 'users/following_and_follower.html')
+
+def follow(request, to_follow):
+    user_to_follow = CustomUser.objects.filter(username=to_follow).first()
+    if request.user in user_to_follow.followers.all():
+        messages.info(request, "You are already following this user.")
+        return redirect('painter_profile', painter=to_follow)
+    elif request.user.username == to_follow:
+        messages.info(request, "You can't follow yourself.")
+        return redirect('painter_profile', painter=to_follow)
+    else:
+        UserFollowing.objects.create(user_from=request.user, user_to=user_to_follow)
+    
+    return redirect('painter_profile', painter=to_follow)
+
+def unfollow(request, to_unfollow):
+    user_to_unfollow = CustomUser.objects.filter(username=to_unfollow).first()
+    if request.user.username == to_unfollow:
+        messages.info(request, "You can't unfollow yourself.")
+        return redirect('painter_profile', painter=to_unfollow)
+    elif request.user not in user_to_unfollow.following.all():
+        messages.info(request, "You are not following this user.")
+        return redirect('painter_profile', painter=to_unfollow)
+    else:
+        UserFollowing.objects.delete(user_from=request.user, user_to=user_to_unfollow)
+    
+    return redirect('painter_profile', painter=to_unfollow)
+
 def profile_view(request):
     return render(request, 'users/profile.html')
+
+def painter_profile_view(request, painter):
+    context = {
+        'painter': CustomUser.objects.filter(username=painter).first()
+    }
+    return render(request, 'users/painter.html', context)
 
 def logout_view(request):
     logout(request)
