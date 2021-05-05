5dff8c26392909a41d2e38d96ccaad0575a733e7
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-05 23:09:41 +0100

    added event listeners to hide and show tabs

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,44 +1,69 @@
 
 
 $(document).ready(function (){
-    AOS.init();
+    // AOS.init();
 
-    // click to scroll top
-    $('.move-up span').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
-    });
+    // // click to scroll top
+    // $('.move-up span').click(function () {
+    //     $('html, body').animate({
+    //         scrollTop: 0
+    //     }, 1000);
+    // });
 
     $('li.active').removeClass('active');
     $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
 
-    const buttons = document.querySelector('.settings'),
-        buttonsList = buttons.querySelectorAll('button'),
-        totalButtons = buttonsList.length
-    
-    for (let i=0; i<totalButtons; i++){
-        const a = buttonsList[i]
-        a.addEventListener('click', function(){
-            for (let j=0; j<totalButtons; j++){
-                buttonsList[j].classList.remove('active');
-            }
-            this.classList.add('active');
-        })
-    };
 
+    $('#profile').addClass('active');
     $('#profile-edit').show();
     $('#home-feed').hide();
 
     $('#profile').click(function () {
+        $('#profile').addClass('active');
+        $('#feed').removeClass('active');
         $('#home-feed').hide();
         $('#profile-edit').show();
     });
     
     $('#feed').click(function () {
+        $('#feed').addClass('active')
+        $('#profile').removeClass('active')
         $('#profile-edit').hide();
         $('#home-feed').show();
     });
+
     
+    $('#tried').addClass('active')
+    $('#tries').show()
+    $('#comments').hide()
+    $('#add_comment').hide()
+    
+    $('#tried').click(function(){
+        $('#tried').addClass('active')
+        $('#paint_comments').removeClass('active')
+        $('#show_inp').removeClass('active')
+        $('#tries').show()
+        $('#comments').hide()
+        $('#add_comment').hide()
+    });
+    
+    $('#paint_comments').click(function(){
+        $('#paint_comments').addClass('active')
+        $('#tried').removeClass('active')
+        $('#show_inp').removeClass('active')
+        $('#comments').show()
+        $('#tries').hide()
+        $('#add_comment').hide()
+    });
+    
+    $('#show_inp').click(function(){
+        $('#show_inp').addClass('active')
+        $('#tried').removeClass('active')
+        $('#paint_comments').removeClass('active')
+        $('#comments').hide()
+        $('#tries').hide()
+        $('#add_comment').show()
+    });
+
 });
 
