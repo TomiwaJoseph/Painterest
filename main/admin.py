2344fea47f3bceedacc07bce1903c70aa11738f6
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-08 16:54:35 +0100

    added model to admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Paintings
+from .models import Paintings, Message
 
 # Register your models here.
 admin.site.register(Paintings)
+admin.site.register(Message)
