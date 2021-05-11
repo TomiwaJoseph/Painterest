0d65c907c160e6caef6f953ea0a7246251ba37b1
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-11 16:33:28 +0100

    added add painting try form

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,23 @@
 from django import forms
-from .models import Paintings
+from .models import Paintings, PaintTries
 
 
 class AddPainting(forms.ModelForm):
     class Meta:
         model = Paintings
-        fields = ('painting', 'title', 'description')
+        fields = ('painting', 'title', 'description', 'tags')
         widgets = {
             'description': forms.TextInput(
                 attrs={'placeholder': 'Write how the painting made you feel...or not😉😉'}),
             'title': forms.TextInput(
-                attrs={'placeholder': 'Give the painting a title...'})
+                attrs={'placeholder': 'Give the painting a title...'}),
+            'tags': forms.TextInput(
+                attrs={'placeholder': 'figure-painting, portrait, landscape'})
             }
+
+
+class AddPaintingTry(forms.ModelForm):
+    class Meta:
+        model = PaintTries
+        fields = ('tries',)
+
