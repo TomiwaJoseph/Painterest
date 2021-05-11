ac9368d4cc24c10428831429665570614b4206b2
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-11 16:28:22 +0100

    added more Comments and PaintTries models

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Paintings, Message
+from .models import Paintings, Message, Comment, PaintTries
 
 # Register your models here.
 admin.site.register(Paintings)
 admin.site.register(Message)
+admin.site.register(Comment)
+admin.site.register(PaintTries)
