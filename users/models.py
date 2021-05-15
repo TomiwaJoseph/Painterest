a114fbf7cf8ae5417d5d110bbe2940d109c80f57
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-15 23:25:16 +0100

    made some imports and upgraded the save method

--- users/models.py
+++ users/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.conf import settings
 from PIL import Image
+from uuid import uuid4
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
+from django.utils.text import slugify
+
 
 alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
 
@@ -17,23 +23,55 @@ class CustomUser(AbstractUser):
         return self.username
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile', on_delete=models.CASCADE)
-    image = models.ImageField(default='dp.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='90a70ab564f045a48488cd440f3b56e4-dp.jpg', upload_to='profile_pics', blank=True)
     about = models.TextField(default='Write a little bit about yourself here', blank=True)
     website_url = models.CharField(max_length=50, blank=True)
+    feed_tuner = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
     def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
+        img = Image.open(self.image)
 
-        img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            output = BytesIO()
+            current_file_ext = self.image.name.split('.')[-1]
+            ext = ''
+            if current_file_ext == 'jpg' or current_file_ext == 'jfif':
+                ext = 'JPEG'
+            else:
+                ext = current_file_ext
+            img = img.resize((300, 300))
+
+            img.save(output, format='{}'.format(ext).upper(), quality=100)
+            output.seek(0)
+
+            the_hex = uuid4().hex
+            self.image = InMemoryUploadedFile(output, "ImageField",
+                f'{the_hex}.jpg', 'image/jpeg',
+                sys.getsizeof(output), None)
+
+        super().save(*args, **kwargs)
 
 
 class UserFollowing(models.Model):
