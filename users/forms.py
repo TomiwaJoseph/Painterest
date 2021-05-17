1fb24f9fe8e8c33e6460d2915567caab360fda98
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-17 12:53:07 +0100

    changed imagefield to remove currently and show error messages

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 from django import forms
-from .models import Profile
+from .models import Profile, Category
 
+# choices = Category.objects.all().values_list('name', 'name')
+# choice_list = []
+
+# for item in choices:
+#     choice_list.append(item)
 
 class RegisterForm(UserCreationForm):
     class Meta:
@@ -19,13 +24,24 @@ class UserUpdateForm(forms.ModelForm):
         model = get_user_model()
         fields = ('username','email', 'first_name', 'last_name')
 
+
 class ProfileUpdateForm(forms.ModelForm):
+    image = forms.ImageField(required=False,
+        widget=forms.FileInput, error_messages={'invalid':('Image files only')})
     class Meta:
         model = Profile
         fields = ('image','about', 'website_url')
         widgets = {
             'website_url': forms.TextInput(
                 attrs={'placeholder': 'Write your website url here...'}),
-            # 'image': forms.ImageField(
-            #     attrs={'class': 'form-control'}),
         }
+
+
+# class CategoryForm(forms.ModelForm):
+#     name = forms.ModelMultipleChoiceField(queryset=Category.objects.all())
+#     class Meta:
+#         model = Category
+#         fields = ('name',)
+#         widgets = {
+#             'name': forms.Select(choices=choice_list)
+#         }
