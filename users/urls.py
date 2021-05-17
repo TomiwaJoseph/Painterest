c29a65b429bba11763a07db3f89654525a98f50b
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-05-17 12:55:08 +0100

    renamed some paths added new paths

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
-
-from . views import (LoginView, RegisterView, profile_view,
-    logout_view, settings_view, painter_profile_view, follow,
-    unfollow, following_and_follower)
+from . import views 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', profile_view, name='profile'),
-    path('painter_profile/<slug:painter>/', painter_profile_view, name='painter_profile'),
-    path('<slug:painter>/following_and_follower/', following_and_follower, name='following_and_follower'),
-    path('logout/', logout_view, name='logout'),
-    path('follow/<slug:to_follow>/', follow, name='follow'),
-    path('unfollow/<slug:to_unfollow>/', unfollow, name='unfollow'),
-    path('settings/', settings_view, name='settings'),
+    path('login/', views.MyLoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('profile/', views.profile_view, name='profile'),
+    path('painter_profile/<slug:painter>/', views.painter_profile_view, name='painter_profile'),
+    path('<slug:painter>/following_and_follower/', views.following_and_follower, name='following_and_follower'),
+    path('logout/', views.logout_view, name='logout'),
+    path('follow/<slug:to_follow>/', views.follow, name='follow'),
+    path('unfollow/<slug:to_unfollow>/', views.unfollow, name='unfollow'),
+    path('settings/', views.settings_view, name='settings'),
+    path('feed_tuner/', views.feed_tuner, name='feed_tuner'),
 ]
