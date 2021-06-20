21033334c55f517918500de0351a82277770b50c
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-06-20 19:58:21 +0100

    cleaned up code

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,14 +1,6 @@
-
-
-$(document).ready(function (){
+$(document).ready(function () {
     // AOS.init();
 
-    // // click to scroll top
-    // $('.move-up span').click(function () {
-    //     $('html, body').animate({
-    //         scrollTop: 0
-    //     }, 1000);
-    // });
 
     $('li.active').removeClass('active');
     $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
@@ -17,14 +9,14 @@ $(document).ready(function (){
     $('#profile').addClass('active');
     $('#profile-edit').show();
     $('#home-feed').hide();
-    
+
     $('#profile').click(function () {
         $('#profile').addClass('active');
         $('#feed').removeClass('active');
         $('#home-feed').hide();
         $('#profile-edit').show();
     });
-    
+
     $('#feed').click(function () {
         $('#feed').addClass('active')
         $('#profile').removeClass('active')
@@ -32,14 +24,14 @@ $(document).ready(function (){
         $('#home-feed').show();
     });
 
-    
+
     $('#tried').addClass('active')
     $('#tries').show()
     $('#comments').hide()
     $('#add_comment').hide()
     $('#add_try').hide()
-    
-    $('#tried').click(function(){
+
+    $('#tried').click(function () {
         $('#tried').addClass('active')
         $('#paint_comments').removeClass('active')
         $('#show_inp').removeClass('active')
@@ -48,8 +40,8 @@ $(document).ready(function (){
         $('#add_comment').hide()
         $('#add_try').hide()
     });
-    
-    $('#paint_comments').click(function(){
+
+    $('#paint_comments').click(function () {
         $('#paint_comments').addClass('active')
         $('#tried').removeClass('active')
         $('#show_inp').removeClass('active')
@@ -58,8 +50,8 @@ $(document).ready(function (){
         $('#add_comment').hide()
         $('#add_try').hide()
     });
-    
-    $('#show_inp').click(function(){
+
+    $('#show_inp').click(function () {
         $('#show_inp').addClass('active')
         $('#tried').removeClass('active')
         $('#paint_comments').removeClass('active')
@@ -69,7 +61,7 @@ $(document).ready(function (){
         $('#add_try').hide()
     });
 
-    $('.add_pho').click(function(){
+    $('.add_pho').click(function () {
         $('#add_try').show()
         $('#show_inp').removeClass('active')
         $('#tried').removeClass('active')
@@ -81,7 +73,7 @@ $(document).ready(function (){
 
     $('#message_div').hide()
 
-    $('#send_button').click(function(){
+    $('#send_button').click(function () {
         $('#message_div').show()
     })
 
@@ -100,22 +92,22 @@ const filter = document.querySelector('.all_filter_but'),
     all_filter_buttons = filter.querySelectorAll('button'),
     totalButtons = all_filter_buttons.length;
 
-for(let i=0; i<totalButtons; i++){
+for (let i = 0; i < totalButtons; i++) {
     const a = all_filter_buttons[i];
-    a.addEventListener('click', function(){
-        for(let j=0; j<totalButtons; j++){
-            all_filter_buttons[j].classList.remove('active');            
+    a.addEventListener('click', function () {
+        for (let j = 0; j < totalButtons; j++) {
+            all_filter_buttons[j].classList.remove('active');
         }
         this.classList.add('active');
-        if(this.innerHTML == 'Read'){
+        if (this.innerHTML == 'Read') {
             $('#read_filter_section').show()
             $('#unread_filter_section').hide()
             $('#all_filter_section').hide()
-        }else if(this.innerHTML == 'Unread'){
+        } else if (this.innerHTML == 'Unread') {
             $('#unread_filter_section').show()
             $('#all_filter_section').hide()
             $('#read_filter_section').hide()
-        }else{
+        } else {
             $('#all_filter_section').show()
             $('#unread_filter_section').hide()
             $('#read_filter_section').hide()
