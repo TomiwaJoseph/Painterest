5ffd6132a2de358ffb7fafe192c76ee92c09dfb6
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-06-20 20:02:31 +0100

    added an action for to create notification for each follower

--- actions/utils.py
+++ actions/utils.py
@@ -4,21 +4,18 @@ from django.contrib.contenttypes.models import ContentType
 from .models import Action
 
 
-def create_action(user, verb, target=None):
-    # check for any similar action made in the last minute
+def create_action(user, verb, target, action_for):
     now = timezone.now()
-    last_minute = now - datetime.timedelta(seconds=60)
-    similar_actions = Action.objects.filter(user_id=user.id,
-        verb= verb,
-        created__gte=last_minute)
-    if target:
-        target_ct = ContentType.objects.get_for_model(target)
-        similar_actions = similar_actions.filter(
-            target_ct=target_ct,
-            target_id=target.id)
-    if not similar_actions:
-        # no existing actions found
-        action = Action(user=user, verb=verb, target=target)
+    last_minute = now - datetime.timedelta(minutes=60)
+
+    if verb == 'started following':
+        similar_actions = Action.objects.filter(user_id=user.id,
+            action_for= action_for, created__gte=last_minute)
+        if not similar_actions:
+            action = Action(user=user, verb=verb, target=target, action_for=action_for)
+            action.save()
+            return True
+    else:
+        action = Action(user=user, verb=verb, target=target, action_for=action_for)
         action.save()
         return True
-    return False
