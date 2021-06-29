69a6a069e9cc5209b35dd77958956088dc3567d9
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-06-29 22:54:28 +0100

    changed register view to fbv

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.MyLoginView.as_view(redirect_authenticated_user=True), name='login'),
-    path('register/', views.RegisterView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('painter_profile/<slug:painter>/', views.painter_profile_view, name='painter_profile'),
     path('<slug:painter>/following_and_follower/', views.following_and_follower, name='following_and_follower'),
