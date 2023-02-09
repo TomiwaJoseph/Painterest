130066575994d91b0bd3654ae1f0611986305040
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-02-09 03:48:08 +0100

    added artist_name to form

--- main/forms.py
+++ main/forms.py
@@ -3,19 +3,22 @@ from .models import Paintings, PaintTries
 
 
 class AddPainting(forms.ModelForm):
+    painting = forms.ImageField(required=True)
+
     class Meta:
         model = Paintings
-        fields = ('painting', 'title', 'tags')
+        fields = ('painting', 'title', 'artist_name', 'tags')
         widgets = {
             'title': forms.TextInput(
                 attrs={'placeholder': 'Give the painting a title...'}),
             'tags': forms.TextInput(
-                attrs={'placeholder': 'figure-painting, portrait, landscape'})
-            }
+                attrs={'placeholder': 'figure-painting, portrait, landscape'}),
+            'artist_name': forms.TextInput(
+                attrs={'placeholder': 'Tomiwa Joseph'})
+        }
 
 
 class AddPaintingTry(forms.ModelForm):
     class Meta:
         model = PaintTries
         fields = ('tries',)
-
