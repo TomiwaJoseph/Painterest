52f99b21334efc9ceacf8fdd538367ada2f143a2
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-02-13 00:38:24 +0100

    added static server to urls

--- painterest/urls.py
+++ painterest/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('users.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT})
 ]
 
 if settings.DEBUG:
